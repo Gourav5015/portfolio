a4569615d65fd8138de691ffa25e3243db2f4387
gourav <aggourav5015@gmail.com>
2022-01-10 17:17:58 +0530

    portfolio

--- src/components/nav.js
+++ src/components/nav.js
@@ -9,9 +9,9 @@ const Nav = () => {
             <div className="cancel" onClick={()=>setNavbar(false)}>X</div>
         <nav>
             <li onClick={()=>setNavbar(false)}><a href="/">home</a></li>
-            <li onClick={()=>setNavbar(false)}><a href="/about">about me</a></li>
-            <li onClick={()=>setNavbar(false)}><a href="/skills">skills</a></li>
-            <li onClick={()=>setNavbar(false)}><a href="/contact">contact me</a></li>
+            <li onClick={()=>setNavbar(false)}><a href="/#about">about me</a></li>
+            <li onClick={()=>setNavbar(false)}><a href="/#skills">skills</a></li>
+            <li onClick={()=>setNavbar(false)}><a href="/#contact">contact me</a></li>
         </nav>
         </div>:<img src={Menu} alt="menu" className='menu' onClick={()=>{setNavbar(true)}}/>}  
         </>
